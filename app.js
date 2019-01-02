490840bb6e14789307614973ba1f828537129ffb
Henry Unite <henryunite@Henrys-MacBook-Pro.local>
2019-01-02 05:04:28 -0500

    routes

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('*', (req, res) => {
-  res.send('Server Online!')
-})
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://henry:henry5398@ds263493.mlab.com:63493/heroku_8p4bgrc8', {useNewUrlParser: true});
 
-app.listen(port, () => {console.log("Server Online!")})
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  const battlegear = require('./routes/battlegear');
+  app.use('/battlegear', battlegear)
+
+  const creatures = require('./routes/creatures');
+  app.use('/creatures', creatures);
+
+  const attacks = require('./routes/attacks');
+  app.use('/attacks', attacks)
+
+  const mugic = require('./routes/mugic');
+  app.use('/mugic', mugic)
+
+  const locations = require('./routes/locations')
+  app.use('/locations', locations)
+
+  app.listen(port, () => {console.log("Server Online!")})
+
+})
