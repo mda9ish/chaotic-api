a5f45b4230e8577166eb7987c30e2d70810cf5f4
unitehenry <hunite@striven.com>
2019-08-25 14:27:43 -0400

    docs

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: 'Chaotic TCG API', // Title for your website.
   tagline: 'Documentation',
-  url: 'http://chaoticga.me/', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://unitehenry.github.io', // Your website URL
+  baseUrl: '/chaotic-api/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
   projectName: 'chaotic-api',
-  organizationName: 'chaotic-api',
+  organizationName: 'unitehenry',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
